c369f5a2d6cc083fec3db620c07bcdb5a343300c
Benny <benjamin.graefe@dci-student.org>
2025-11-25 15:20:09 +0100

    footer-design

--- frontend/src/components/SharkList.jsx
+++ frontend/src/components/SharkList.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import SharkCard from "./SharkCard";
 
-const SharkList = ({ sharks }) => {
+const SkeletonCard = () => (
+  <div className="shark-card skeleton-card">
+    <div className="skeleton skeleton-image"></div>
+    <div className="skeleton skeleton-title"></div>
+    <div className="skeleton skeleton-text"></div>
+    <div className="skeleton skeleton-text short"></div>
+  </div>
+);
+
+const SharkList = ({ sharks, loading = false }) => {
+  if (loading) {
+    return (
+      <div className="shark-list">
+        {[...Array(6)].map((_, index) => (
+          <SkeletonCard key={index} />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="shark-list">
       {sharks.map((shark) => (
